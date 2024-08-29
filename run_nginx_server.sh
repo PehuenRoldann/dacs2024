3c02491669e106834eb117333e016edae9502593
Pehuen <roldanpehuen@gmail.com>
2024-08-29 15:10:45 -0300

    Removed unused .sh

--- run_nginx_server.sh
+++
@@ -1,6 +0,0 @@
-docker run --name dacs2024-nginx \
-	-v ./web-app:/usr/share/nginx/html:ro \
-	-v ./nginx.conf:/etc/nginx/nginx.conf:ro \
-	-v ./dist/urbancheck-front-app/browser/:/web-app:ro \
-	-d -p 80:80 nginx
-
