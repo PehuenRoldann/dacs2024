512dc033961f5070804588e5733fbf5404cb4972
Pehuen <roldanpehuen@gmail.com>
2024-09-25 18:23:26 -0300

    Nose que cambié acá, pero lo comiteo

--- urbancheck-front-app/src/app/app.module.ts
+++ urbancheck-front-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginViewComponent } from './components/login-view/login-view.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from './core/init/keycloak-init.factory';
+import { ApiService } from './core/services/apiservice.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,10 +14,19 @@ import { LoginViewComponent } from './components/login-view/login-view.component
   ],
   imports: [
     BrowserModule,
+    KeycloakAngularModule,
     AppRoutingModule,
-    LoginViewComponent
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+    ApiService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ urbancheck-front-app/src/environments/environment.localhost.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  keycloak: {
+    url: "http://localhost:8888",
+    realm: "urbancheck",
+    clientId: "urbancheck-fe",
+  },
+  backendForFrontendUrl: "/api",
+};
